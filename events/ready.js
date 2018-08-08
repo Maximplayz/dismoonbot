237d0a996f87bf791a060f03cc3fa63dedb15eb8
Lorey <helloitspichu@gmail.com>
2018-08-08 20:04:25 +0200

    !!!

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = (client) => {
 
-  console.log(`\nBot is ready!\n\nOn ${client.guilds.count} server/s`)
+  console.log(`\nBot is ready at ${client.readyAt}!\n`)
   client.user.setActivity('im a bot lol')
   client.user.setStatus('dnd')
 
