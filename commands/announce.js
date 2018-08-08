fc46bb759432a7ea37ded9233bbf4ddd29379344
Lorey <helloitspichu@gmail.com>
2018-08-08 02:24:21 +0200

    $$$

--- commands/announce.js
+++ commands/announce.js
@@ -2,11 +2,9 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
 
-  console.log(message.author.member)
-  console.log(message.author.member.roles)
 
 }
 
 module.exports.help = {
-    name: "test"
+    name: "ping"
 }
