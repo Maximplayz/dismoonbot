ee7b779716e98fb2e1aab7307275991ee3931ca9
Lorey <helloitspichu@gmail.com>
2018-08-08 03:49:39 +0200

    please work

--- commands/announce.js
+++ commands/announce.js
@@ -22,7 +22,7 @@ module.exports.run = async (bot, message, args) => {
     let embed = new Discord.RichEmbed()
     .setColor('#30E4FF')
     .setTitle(title)
-    .setFooter("", message.author.avatarURL)
+    .setFooter(" ", message.author.avatarURL)
     .setDescription(args.join(' '))
     .setTimestamp()
     message.channel.send(embed)
