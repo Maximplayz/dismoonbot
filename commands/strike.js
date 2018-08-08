ac22594640db7989cf8fcdf5ddfd317d83499ba0
Lorey <helloitspichu@gmail.com>
2018-08-08 23:23:03 +0200

    sdas

--- commands/strike.js
+++ commands/strike.js
@@ -13,6 +13,10 @@ exports.run = (client, message, args) => {
   var hcorole = message.guild.roles.find("name", "HCOs").id;
   var forole = message.guild.roles.find("name", "FOs").id;
 
+  if(!reason){
+    reason = "No reason provided."
+  }
+
   if(authormem.roles.has(corole) || authormem.roles.has(hcorole) || authormem.roles.has(forole)){
 
   var embed = new Discord.RichEmbed()
@@ -25,6 +29,7 @@ exports.run = (client, message, args) => {
     message.channel.send('**Mentioned user already has __3 Strikes__!**')
   } else if(memtostrike.roles.has(strike2.id)){
     embed.addField("**Strike Number**", strike3.name)
+    memtostrike.send(`Hey there. **${striker}** just striked you for: "**${reason}**"\n\nYou now have **3 Strikes** therefore you are getting kicked if you are kickable.`)
     if(memtostrike.kickable){
       embed.addField("**3 Strikes Reached**", "Due to reaching 3 strikes the unit will be kicked now.")
       memtostrike.kick("3 Strikes Reached")
@@ -36,11 +41,13 @@ exports.run = (client, message, args) => {
     embed.addField("**Strike Number**", strike2.name)
     embed.setColor('#ee7300')
     memtostrike.addRole(strike2)
+    memtostrike.send(`Hey there. **${striker}** just striked you for: "**${reason}**"\n\nYou now have **2 Strikes**`)
     message.channel.send(embed)
   } else {
     embed.addField("**Strike Number**", strike1.name)
     embed.setColor('#ffdb00')
     memtostrike.addRole(strike1)
+    memtostrike.send(`Hey there. **${striker}** just striked you for: "**${reason}**"\n\nYou now have **1 Strike**`)
     message.channel.send(embed)
   }
 
