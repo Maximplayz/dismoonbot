bde2bf5f2bdd356e34a7d34e12191902178d50d0
Lorey <helloitspichu@gmail.com>
2018-08-21 21:37:33 +0200

    hehehuhuhhaha

---
+++ commands/vote.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+
+    var authmem = message.guild.members.get(message.author.id);
+    var vtext = args.join(" ")
+    var admrole = message.guild.roles.find("name", "Discord Admin").id;
+
+    if(authmem.roles.has(admrole)) {
+      clr = "#182179";
+    } else {
+      clr = authmem.highestRole.color;
+    }
+
+    var embed = new Discord.RichEmbed()
+    .setTitle("New Vote:")
+    .setColor(clr)
+    .setDescription(vtext)
+    .setFooter(`Vote by ${authormem.nickname}`, message.author.avatarURL)
+    message.channel.send(embed).then(m => {
+      m.react('✅')
+      m.react('❎')
+    })
+
+}
